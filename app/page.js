21960008a81f8e84f3f7c14843767377c955f86c
nevaybnina <smile.nulia@gmail.com>
2026-09-08 23:55:26 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,18 @@
 import { events } from '../data/events';
 import EventCountdown from '../components/EventCountdown';
 
+const event = events[0];
+const description = `${event.eventSub} Отсчёт до ${event.displayDate} по Санкт-Петербургу.`;
+
+export const metadata = {
+  title: event.eventName,
+  description,
+  openGraph: {
+    title: event.eventName,
+    description,
+  },
+};
+
 export default function Home() {
-  const event = events[0];
   return <EventCountdown event={event} />;
 }
