40a802bb2affd32b3f87e7753bc1359c38c97137
nevaybnina <smile.nulia@gmail.com>
2026-09-08 22:30:20 +0300

    Delete page.js

--- page.js
+++
@@ -1,7 +0,0 @@
-import { events } from '../data/events';
-import EventCountdown from '../components/EventCountdown';
-
-export default function Home() {
-  const event = events[0];
-  return <EventCountdown event={event} />;
-}
